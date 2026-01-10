07dd48312be46a7eb40daea6f8552f19710467c6
Gabrielle <154719095+gabriellegtw@users.noreply.github.com>
2026-01-10 14:02:15 -0500

    Adding components for authentication page

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,12 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(
   <React.StrictMode>
-    <App />
+    {/* <Auth0Provider
+      domain={import.meta.env.VITE_AUTH0_DOMAIN}
+      clientId={import.meta.env.VITE_AUTH0_CLIENT_ID}
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}
+    > */}
+      <App />
+    {/* </Auth0Provider> */}
   </React.StrictMode>
 );
 
