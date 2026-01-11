8ad6c03c7de6e8fa6c9341f9bbece7a2bb6d7fc6
Gabrielle <154719095+gabriellegtw@users.noreply.github.com>
2026-01-10 20:16:26 -0500

    Add add patients API

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -6,6 +6,9 @@ const userController = require('../controllers/usercontrollers');
 // POST /api/users/register - Register new user
 router.post('/register', userController.createUser); // Uses createUser controller
 
+// POST /api/users/addpatient - Add a new patient (by caregiver)
+router.post('/addpatient', userController.createPatient);
+
 // (ToDO later!!) Apply authentication middleware to all routes below
 // router.use(authenticate);
 
